b17732ffa9d393e6253b5a0823a6bbc7eae49cc8
Ansh Kushwaha <anshkushwaha2001@gmail.com>
2024-08-06 10:32:01 +0530

    Updates

---
+++ precache-manifest.c4b360c2c481bfddccffd348125c0f38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4af96222c1372dad904e8f1fe417b859",
+    "url": "/flame-cai/ai_playground/index.html"
+  },
+  {
+    "revision": "735e5af14f6c0e545885",
+    "url": "/flame-cai/ai_playground/static/css/main.86098ffc.chunk.css"
+  },
+  {
+    "revision": "62f7ecb017e711a09b1e",
+    "url": "/flame-cai/ai_playground/static/js/2.b5dbc987.chunk.js"
+  },
+  {
+    "revision": "9cedf2ca1bbc3288ea502c2be17fce46",
+    "url": "/flame-cai/ai_playground/static/js/2.b5dbc987.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "178581334fa3e882e978",
+    "url": "/flame-cai/ai_playground/static/js/3.50f9ff74.chunk.js"
+  },
+  {
+    "revision": "735e5af14f6c0e545885",
+    "url": "/flame-cai/ai_playground/static/js/main.ed2d1ca6.chunk.js"
+  },
+  {
+    "revision": "62ed39ffa3a107ad1f10",
+    "url": "/flame-cai/ai_playground/static/js/runtime-main.d63384d4.js"
+  }
+]);
